1c535f10480d0735d6f055886793d5e4afb11c00
Chander Parkash <chanderparkash179@gmail.com>
2024-01-19 02:32:35 +0500

    [BNA-02] - connected mongodb & added packages

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
+// package imports
+import dotenv from 'dotenv';
 import express from 'express';
+
+// file imports
+import connectDB from './database/db.connection.js';
+
+dotenv.config({ path: './env' });
+
+connectDB();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => { console.log(`server running on PORT : ${PORT}`) })
+app.listen(PORT, () => { console.log(`server running on PORT: [${PORT}]`) });
